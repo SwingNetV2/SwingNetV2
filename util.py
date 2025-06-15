2a4b88e4df9123da8e5c222285c142dc80ac7d37
anjihyeon527 <anjihyeon527@gmail.com>
2025-06-15 17:44:41 +0900

    Create util.py

---
+++ util.py
@@ -0,0 +1,76 @@
+class AverageMeter(object):
+    """Computes and stores the average and current value"""
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count
+
+
+def correct_preds(probs, labels, tol=-1):
+    """
+    Gets correct events in full-length sequence using tolerance based on number of frames from address to impact.
+    Used during validation only.
+    :param probs: (sequence_length, 9)
+    :param labels: (sequence_length,)
+    :return: array indicating correct events in predicted sequence (8,)
+    """
+
+    events = np.where(labels < 8)[0]
+    preds = np.zeros(len(events))
+    if tol == -1:
+        tol = int(max(np.round((events[5] - events[0])/30), 1))
+    for i in range(len(events)):
+        preds[i] = np.argsort(probs[:, i])[-1]
+    deltas = np.abs(events-preds)
+    correct = (deltas <= tol).astype(np.uint8)
+    return events, preds, deltas, tol, correct
+
+
+def freeze_layers(num_freeze, net):
+    # print("Freezing {:2d} layers".format(num_freeze))
+    i = 1
+    for child in net.children():
+        if i ==1:
+            j = 1
+            for child_child in child.children():
+                if j <= num_freeze:
+                    for param in child_child.parameters():
+                        param.requires_grad = False
+                j += 1
+        i += 1
+
+
+transform_video_frames = A.Compose([ A.Resize(height=160, width=160),
+                          A.HorizontalFlip(p=0.5),
+                          A.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225], max_pixel_value=255.0), # 0605
+                          A.ToTensorV2(),
+])
+
+
+class ToTensor(object):
+    """Convert ndarrays in sample to Tensors."""
+    def __call__(self, sample):
+        images= sample['images']
+        images = images.transpose((0, 3, 1, 2))
+        return {'images': torch.from_numpy(images).float().div(255.)}
+
+
+class Normalize(object):
+    def __init__(self, mean, std):
+        self.mean = torch.tensor(mean, dtype=torch.float32)
+        self.std = torch.tensor(std, dtype=torch.float32)
+
+    def __call__(self, sample):
+        images = sample['images']
+        images.sub_(self.mean[None, :, None, None]).div_(self.std[None, :, None, None])
+        return {'images': images}
