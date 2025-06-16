baf2f5f9a7030460608bd97183392df5e07e4a2a
anjihyeon527 <anjihyeon527@gmail.com>
2025-06-17 01:58:03 +0900

    Update predict.py

--- predict.py
+++ predict.py
@@ -1,117 +1,29 @@
-import sys
+import os
+import os.path as osp
 import argparse
-import math
-
-import numpy as np
 import cv2
 import torch
 import torch.nn.functional as F
+import numpy as np
+import math
 from torch.utils.data import Dataset, DataLoader
 from torchvision import transforms
 from PIL import Image
-
 from google.colab.patches import cv2_imshow
+
 import mediapipe as mp
+from predict_utils import *
+from model.event_detector import EventDetector
 from mediapipe import solutions
 from mediapipe.tasks import python
 from mediapipe.tasks.python import vision
 from mediapipe.framework.formats import landmark_pb2
 
-from model.event_detector import EventDetector
-
-
-class ToTensor(object):
-    """Convert ndarrays in sample to Tensors."""
-    def __call__(self, sample):
-        images= sample['images']
-        images = images.transpose((0, 3, 1, 2))
-        return {'images': torch.from_numpy(images).float().div(255.)}
-
-
-class Normalize(object):
-    def __init__(self, mean, std):
-        self.mean = torch.tensor(mean, dtype=torch.float32)
-        self.std = torch.tensor(std, dtype=torch.float32)
-
-    def __call__(self, sample):
-        images = sample['images']
-        images.sub_(self.mean[None, :, None, None]).div_(self.std[None, :, None, None])
-        return {'images': images}
-
-event_names = {
-    0: 'Address',
-    1: 'Toe-up',
-    2: 'Mid-backswing (arm parallel)',
-    3: 'Top',
-    4: 'Mid-downswing (arm parallel)',
-    5: 'Impact',
-    6: 'Mid-follow-through (shaft parallel)',
-    7: 'Finish'
-}
 
 model = EventDetector(lstm_layers=1,
                       lstm_hidden=256,
                       bidirectional=True,
                       dropout=False,)
-
-
-
-def draw_landmarks_on_image(rgb_image, detection_result):
-  pose_landmarks_list = detection_result.pose_landmarks
-  annotated_image = np.copy(rgb_image)
-
-  # Loop through the detected poses to visualize.
-  for idx in range(len(pose_landmarks_list)):
-    pose_landmarks = pose_landmarks_list[idx]
-
-    # Draw the pose landmarks.
-    pose_landmarks_proto = landmark_pb2.NormalizedLandmarkList()
-    pose_landmarks_proto.landmark.extend([
-      landmark_pb2.NormalizedLandmark(x=landmark.x, y=landmark.y, z=landmark.z) for landmark in pose_landmarks
-    ])
-    solutions.drawing_utils.draw_landmarks(
-      annotated_image,
-      pose_landmarks_proto,
-      solutions.pose.POSE_CONNECTIONS,
-      solutions.drawing_styles.get_default_pose_landmarks_style())
-  return annotated_image
-
-def calculate_angle( a, b, c):
-        radians = math.atan2(c.y - b.y, c.x - b.x) - math.atan2(a.y - b.y, a.x - b.x)
-        angle = math.degrees(radians)
-        angle = abs(angle)
-        if angle > 180:
-            angle = 360 - angle
-        return angle
-
-# Calculates the angle between two points and one of the axes
-def calculate_angle2( x1, y1, x2, y2, axis='x', orientation='right'):
-    if (math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2) * x1) != 0:
-        if axis == 'x':
-            theta = math.acos((x2 - x1) * (-x1) / (math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2) * x1))
-        elif axis == 'y':
-            theta = math.acos((y2 - y1) * (-y1) / (math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2) * y1))
-        else:
-            raise ValueError("Invalid axis, use 'x' or 'y'")
-
-        if orientation == 'right':
-            angle = int(180 / math.pi) * theta
-        elif orientation == 'left':
-            angle = 180 - int(180 / math.pi) * theta
-        else:
-            raise ValueError("Invalid orientation, use 'left' or 'right'")
-    else:
-        return 0
-
-    return angle
-
-# Calculates the midpoint between two points
-def middle_point(a, b):
-    midpoint_x = (a.x + b.x) / 2
-    midpoint_y = (a.y + b.y) / 2
-    return midpoint_x, midpoint_y
-
-
 
 class SampleVideo(Dataset):
     def __init__(self, path, input_size=160, transform=None):
@@ -202,12 +114,12 @@ if __name__ == '__main__':
         cap.set(cv2.CAP_PROP_POS_FRAMES, e)
         _, img = cap.read()
         img_pil = Image.fromarray(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
-        img_pil.save(f'output2/image{e}.jpg')
+        img_pil.save(f'output/image{e}.jpg')
         cv2.putText(img, '{} , {}'.format(event_names[i], format(e)), (20, 50), cv2.FONT_HERSHEY_DUPLEX, 0.75, (0, 0, 255), 2)
-        cv2.imshow(img)
+        cv2_imshow(img)
         cv2.waitKey(0)
         cv2.destroyAllWindows()
-        image = mp.Image.create_from_file(f'output2/image{e}.jpg')
+        image = mp.Image.create_from_file(f'output/image{e}.jpg')
 
         #Create an PoseLandmarker object.
         base_options = python.BaseOptions(model_asset_path='pose_landmarker.task')
@@ -293,8 +205,7 @@ if __name__ == '__main__':
             cv2.putText(image_np, f'Arm Angle: {arm_angle:.2f}', (10, 145), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (128, 0, 128), 2)
 
             pil_image = Image.fromarray(image_np.astype(np.uint8))
-            pil_image.save(f'output2/output{e}.jpg') # Save with event frame number
-            cv2.imshow(image_np)
+            pil_image.save(f'output/output{e}.jpg') # Save with event frame number
+            cv2_imshow(image_np)
             cv2.waitKey(0)
             cv2.destroyAllWindows()
-            
